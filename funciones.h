6ea16f534f270ad6617456d1f7d19821bade84e8
LucasFcode <lucasfer.flores@gmail.com>
2024-11-13 20:38:20 -0300

    funciones.h

--- funciones.h
+++ funciones.h
@@ -1,19 +1,25 @@
 #pragma once
 #include <string>
+#include <cstdio>
 using namespace std;
 
 
 void seleccion_Opcion ( int opcion );
 
-//int seleccionarOpcion();
-//void ejecutarOpcion (int opcion);
+void estadisticaGeneral();
+void estadisticaunJugador();
+void estadisticaMultiplayer();
+
 
 void partidaUnJugador();
-void modo_unjugador(string nombreJugador);
+void modo_unjugador (string nombreJugador);
+
+
 
 void partidaMultiplayer();
 void multiplayer(string nombreJugador);
-void saludar(string nombreJugador);
+void saludar (string nombreJugador);
 int tirarDados();
-void guardarEstadistica(string nombreJugador1, string nombreJugador2, int puntosGanador1, int puntosGanador2);
-void mostrarEstadistica();
+bool dadosIguales(int dados[], int cantidadDados);
+void creditos();
+
